3a6167a64e1eb77af0f5975e4bd196d032c166f1
Sivert Sømme Sagmo <au549312@uni.au.dk>
2019-11-25 12:36:30 +0100

    run error fixed

--- MicrowaveOvenClasses/Boundary/Timer.cs
+++ MicrowaveOvenClasses/Boundary/Timer.cs
@@ -43,7 +43,7 @@ namespace MicrowaveOvenClasses.Boundary
         {
             // One tick has passed
             // Do what I should
-            TimeRemaining -= 1000;
+            TimeRemaining -= 1;
             TimerTick?.Invoke(this, EventArgs.Empty);
 
             if (TimeRemaining <= 0)
