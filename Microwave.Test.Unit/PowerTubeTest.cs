19c1af9906594e186229042990187197eaec6d0e
Saeed Soltani <au583047@uni.au.dk>
2019-11-25 14:53:21 +0100

    rettet unit test på powertube

--- Microwave.Test.Unit/PowerTubeTest.cs
+++ Microwave.Test.Unit/PowerTubeTest.cs
@@ -22,7 +22,7 @@ namespace Microwave.Test.Unit
         public void TurnOn_WasOff_CorrectOutput()
         {
             uut.TurnOn(50);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("50 %")));
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("50 W")));
         }
 
         [Test]
@@ -56,7 +56,7 @@ namespace Microwave.Test.Unit
         [Test]
         public void TurnOn_HighPower_ThrowsException()
         {
-            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(101));
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(701));
         }
 
         [Test]
