d1df726bf7821da1738cb50a80251723ba87760f
Saeed Soltani <au583047@uni.au.dk>
2019-11-28 13:44:10 +0100

    opsætningish

---
+++ Microwave.Test.Integration/IT6_Testing_CC_Dependencies_PowerTube_UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT6_Testing_CC_Dependencies_PowerTube_UI
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
