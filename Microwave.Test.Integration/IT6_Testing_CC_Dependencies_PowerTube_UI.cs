0b0546f86269f94615a7a2cdee170e1f46a65a4a
Saeed Soltani <au583047@uni.au.dk>
2019-11-28 22:29:09 +0100

    en test mere done

--- Microwave.Test.Integration/IT6_Testing_CC_Dependencies_PowerTube_UI.cs
+++ Microwave.Test.Integration/IT6_Testing_CC_Dependencies_PowerTube_UI.cs
@@ -4,55 +4,65 @@ using NUnit.Framework;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
-using NSubstitute.ExceptionExtensions;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     public class IT6_Testing_CC_Dependencies_PowerTube_UI
     {
-        private ICookController _T;
-        private ITimer _timer;
+        private CookController _IT;
         private IPowerTube _powerTube;
-        private IUserInterface _userInterface;
+        private ITimer _timer;
         private IOutput _output;
-        private IButton _pButton;
-        private IButton _tButton;
-        private IButton _scButton;
+        private IDisplay _display;
+        private IUserInterface _userInterface;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _StartCancelButton;
         private IDoor _door;
         private ILight _light;
-        private IDisplay _display;
-        private CookController controller;
 
 
-            [SetUp]
+        [SetUp]
         public void Setup()
         {
-            //classes faked
             _output = Substitute.For<IOutput>();
-            _pButton = Substitute.For<IButton>();
-            _door = Substitute.For<IDoor>();
-            _display = Substitute.For<IDisplay>();
             _light = Substitute.For<ILight>();
-            //classes used
-            _powerTube = new PowerTube(_output);
-            _userInterface = new UserInterface(_pButton,_tButton,_scButton,
-                                                _door,_display,_light,controller);
-            _pButton = new Button();
-            _tButton = new Button();
-            _scButton = new Button();
-            _door = new Door();
-            _light = new Light(_output);
+            _timeButton = Substitute.For<IButton>();
+            _powerButton = Substitute.For<IButton>();
+            _StartCancelButton = Substitute.For<IButton>();
+            _door = Substitute.For<IDoor>();
+            _timer = new MicrowaveOvenClasses.Boundary.Timer();
             _display = new Display(_output);
-            //Class undertest
-            _T = new CookController(_timer, _display, _powerTube, _userInterface);
+            _powerTube = new PowerTube(_output);
+            _IT = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_timeButton,_powerButton,_StartCancelButton,
+                                                _door,_display,_light,_IT);
+            _IT.UI = _userInterface;
+        }
+        //UI Test
+        [Test]
+        public void OnTimeExpiredOutputsCorrect()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _StartCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _IT.OnTimerExpired(this, EventArgs.Empty);
+            _output.Received().OutputLine("Display cleared");
         }
         //powertube test
         [Test]
-        public void Cooking_TimerOnTick_ShowTime()
+        public void CookController_TurnOn_Test()
         {
-            _timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
-            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("Display shows:")));
+            _IT.StartCooking(50, 2);
+            _output.Received().OutputLine(Arg.Is<string>(x => x == "PowerTube works with 50 W"));
+        }
+        [Test]
+        public void CookController_TurnOnAndTurnOff_Test()
+        {
+            _IT.StartCooking(50, 1);
+            _IT.Stop();
+            _output.Received().OutputLine(Arg.Is<string>(x => x == "PowerTube turned off"));
         }
     }
 }
